03070208771bbe4311f334a34e617ee128cc5fd0
Dennis Duka <dukadennis7@gmail.com>
2020-05-25 23:04:55 +0100

    add note

--- public/app.js
+++ public/app.js
@@ -9,6 +9,7 @@ function login() {
             document.getElementById("display-name").innerHTML = `${user.displayName} (${user.email})`;
             document.getElementById("login").remove();
             showCards();
+            createCard();
         }
     });
 }
@@ -21,7 +22,7 @@ function showCards() {
             docs.forEach(function (doc) {
                 document.getElementById("notes").innerHTML +=
                     `
-                <div class="card col" style="width: 18rem;">
+                <div class="card " style="width: 18rem;">
                     <div class="card-body">
                         <h5 class="card-title">${doc.data().title}</h5>
                         <p class="card-text">${doc.data().desc}</p>
@@ -31,3 +32,31 @@ function showCards() {
             });
         });
 };
+
+function createCard() {
+    document.getElementById("create-note").innerHTML +=
+        `
+            <div class="card w-50" style="width: 18rem;">
+                <div class="card-body">
+                <input type="text" id="input-title" placeholder="title">
+                <input type="text" id="input-desc" placeholder="desc">
+                </div>
+                <button id="submit" type="submit" class="btn btn-dark" onclick="addNote()">Add Note</button>
+            </div>
+        `
+}
+
+function addNote() {
+    db = firebase.firestore();
+    db.collection("notes").add({
+        title: `${document.getElementById("input-title").value }`,
+        desc: `${document.getElementById("input-desc").value }`,
+        show: true
+    })
+    .then(function() {
+        console.log("Document successfully written!");
+    })
+    .catch(function(error) {
+        console.error("Error writing document: ", error);
+    });
+}
